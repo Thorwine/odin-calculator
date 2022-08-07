01e1c2d421d797cdec812d0492bca124fb68172c
Thorwine <thorwine69@gmail.com>
2022-08-07 09:43:15 +0200

    Add class constructor Calc with basic math operations

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,38 @@
-console.log('Hello World!');
+class Calc {
+  constructor(operandX, operandY) {
+
+    this.operandX = operandX;
+    this.operandY = operandY;
+
+    this.add = function () {
+      return this.operandX + this.operandY;
+    };
+
+    this.testSubtract = function () {
+      return this.operandX - this.operandY;
+    };
+
+    this.testMultiply = function () {
+      return this.operandX * this.operandY;
+    };
+
+    this.testDivide = function () {
+      return this.operandX / this.operandY;
+    };
+  };
+};
+
+
+// Tests
+
+const testSum = new Calc(3, 4);
+console.log(testSum.add());
+
+const testSub = new Calc(9, 4);
+console.log(testSub.testSubtract());
+
+const testMul = new Calc(5, 5);
+console.log(testMul.testMultiply());
+
+const testDiv = new Calc(25, 5);
+console.log(testDiv.testDivide());
